5a56fddff6e130671f868a246060c011b11b19a3
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-03-06 10:14:40 -0800

    setup grid for skill cards

--- pages/skills.js
+++ pages/skills.js
@@ -10,7 +10,6 @@ import {
     FaAlpinejs,
 } from 'react-icons/fa'
 import { SiAlpinedotjs, SiTailwindcss } from 'react-icons/si'
-// import { SkillsCard } from './SkillsCard'
 import { SkillsCard } from '../components/SkillsCard'
 import Layout from '@/components/layout'
 import { useState } from 'react'
@@ -18,10 +17,7 @@ import { Modal, ModalContents, ModalOpenButton } from '@/components/Modal'
 import ModalRoot from '@/components/ModalRoot'
 import TestModal from '@/components/TestModal'
 import ModalService from '@/services/ModalService'
-import ReactSkill from '@/components/skills/ReactSkill'
-import VueSkill from '@/components/skills/VueSkill'
 import { skillData, icons } from '@/data'
-import Testing from '@/components/Testing'
 
 const Skills = () => {
     const skills = Object.keys(skillData).map((key) => {
@@ -53,7 +49,6 @@ const Skills = () => {
                                 border: '1px solid #61DBFB',
                             }}
                         /> */}
-                        <ReactSkill />
                         {/* <SkillsCard
                             name="Vue"
                             icon={FaVuejs}
@@ -62,7 +57,6 @@ const Skills = () => {
                                 border: '1px solid #42b883',
                             }}
                         /> */}
-                        <VueSkill />
                         <SkillsCard
                             name="Alpine.js"
                             icon={SiAlpinedotjs}
@@ -222,107 +216,44 @@ const Skills = () => {
                 </div>
             </div>
 
-            <div className="flex flex-row items-center justify-between p-2 px-4 align-middle duration-300 ease-in bg-gray-700 border rounded-md shadow-xl cursor-default lg:px-10 md:w-64 hover:scale-105">
-                <p className="text-xs sm:text-sm md:text-xl">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea
-                    explicabo temporibus repellat debitis eveniet quasi
-                    obcaecati officia veniam magni rem!
-                </p>
-                <icon className="text-lg md:text-xl lg:text-6xl" />
-            </div>
             <div>
-                {/* <button
-                    className="px-4 py-2 text-white rounded-md bg-primary"
-                    onClick={() => setShowModal(true)}>
-                    Open Modal
-                </button>
-                <Modal show={showModal} onClose={() => setShowModal(false)}>
-                    <div className="flex flex-col items-center justify-center w-full h-full">
-                        <h1 className="text-2xl font-bold text-center text-primary">
-                            ;alkdjf
-                        </h1>
-                    </div>
-                </Modal> */}
-                {/* <button onClick={addModal}>Open</button>
-                <ModalRoot /> */}
                 <div>
-                    {/* skillData.map((data, key) => {
-                        return( <SkillsCard key={key}>{data.name}</SkillsCard>
-                        )}) */}
+                    <div className="grid w-2/3 grid-cols-1 gap-2 p-2 pt-0 mx-auto sm:w-full lg:p-16 md:p-0 sm:grid-cols-2 md:grid-cols-3 lg:gap-10">
+                        {skillData.map((skill, key) => {
+                            const icon = skill.icon
 
-                    {/* {skillData.map((data, key) => {
-                        ;<SkillsCard
-                            // return <div key={key}>{data.name}</div>
-                            name={data.name}
-                            icon={data.icon}
-                            key={key}
-                        />
-                    })} */}
-                    {/* {skillData.map((data, key) => {
-                        return <SkillsCard key={key}>{data.name}</SkillsCard>
-                    })} */}
-                    {/* use a map through function to go through the skill data with SkillsCard */}
-                    {/* map through the skilldata using skills card */}
-                    {/* {skills.map((skill) => (
-                        <SkillsCard
-                            name={skill.name}
-                            icon={skill.icon}
-                            key={skill.id}
-                        />
-
-                        // <div key={item.id}>{item.name}</div>
-                    ))} */}
-                    {/* map through skills */}
-                    {/* map through the skilldata */}
-                    {/* {skillData.map((data, key) => {
-                        return
-                        ;<SkillsCard key={key}>
-                            <p>{data.name}</p>
-                        </SkillsCard>
-                        // return <SkillsCard key={key}>{data.name}</SkillsCard>
-                    })} */}
-                    {skillData.map((skill, key) => {
-                        const icon = skill.icon
-
-                        return (
-                            <Modal>
-                                <ModalOpenButton>
-                                    <div
-                                        className="flex flex-row items-center justify-between p-2 px-4 align-middle duration-300 ease-in bg-gray-700 border rounded-md shadow-xl cursor-default lg:px-10 md:w-64 hover:scale-105"
-                                        style={skill.style}
-                                        icon={icon.component}
-                                        key={key}>
-                                        <p
-                                            className="text-xs sm:text-sm md:text-xl"
-                                            icon={skill.icons}>
-                                            {skill.name}
-                                        </p>
+                            return (
+                                <Modal>
+                                    <ModalOpenButton>
                                         <div
-                                            className="text-lg md:text-xl lg:text-6xl"
-                                            icon={skill.icon}>
-                                            {/* {skill.icon} */}
+                                            className="flex flex-row items-center justify-between p-2 px-4 align-middle duration-300 ease-in bg-gray-700 border rounded-md shadow-xl cursor-default lg:px-10 md:w-64 hover:scale-105"
+                                            style={skill.style}
+                                            icon={icon.component}
+                                            key={key}>
+                                            <p
+                                                className="text-xs sm:text-sm md:text-xl"
+                                                icon={skill.icons}>
+                                                {skill.name}
+                                            </p>
+                                            <div
+                                                className="text-lg md:text-xl lg:text-6xl"
+                                                icon={skill.icon}>
+                                                {/* {skill.icon} */}
+                                            </div>
+                                            {/* pass the icon property here */}
+                                            {/* {icon.component} */}
+                                            <p className="text-lg md:text-xl lg:text-6xl">
+                                                {icon.component}
+                                            </p>
                                         </div>
-                                        {/* pass the icon property here */}
-                                        {/* {icon.component} */}
-                                        <p className="text-lg md:text-xl lg:text-6xl">
-                                            {icon.component}
-                                        </p>
-                                    </div>
-                                </ModalOpenButton>
-                                <ModalContents title="hi" show={show}>
-                                    {skill.modalContents.body}
-                                </ModalContents>
-                            </Modal>
-                        )
-                        //              <div
-                        //     className="flex flex-row items-center justify-between p-2 px-4 align-middle duration-300 ease-in bg-gray-700 border rounded-md shadow-xl cursor-default lg:px-10 md:w-64 hover:scale-105">
-                        //     <p className="text-xs sm:text-sm md:text-xl">{skill.name}</p>
-                        //     <div className="text-lg md:text-xl lg:text-6xl" >{skill.icon}</div>
-                        // </div>
-                    })}
-                </div>
-                <div>
-                    <Testing skill />
+                                    </ModalOpenButton>
+                                    <ModalContents title="hi" show={show}>
+                                        {skill.modalContents.body}
+                                    </ModalContents>
+                                </Modal>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </Layout>
